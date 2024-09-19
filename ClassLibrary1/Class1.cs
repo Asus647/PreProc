0ee2c142bdae1f116db6f42a5c695dc998434afd
Влад <pompagoipasdo@gmail.com>
2024-09-20 00:43:02 +0300

    чтение и запоминание экселя и цсв

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,4 +1,7 @@
-﻿namespace ClassLibrary1
+﻿using OfficeOpenXml;
+using System.Drawing.Text;
+using System.IO;
+namespace ClassLibrary1
 {
     public static class PreProc
     {
@@ -19,6 +22,28 @@
                 line = reader.ReadLine();
             }
             return output;
+        }
+        public static List<string[]> ReadExcelFile(string path)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            int totalRows = 0;
+            int totalCols = 0;
+            List<string[]> ExcelTable = new List<string[]>();
+            var file = new FileInfo(@path);
+            using (var excelfile = new ExcelPackage(file))
+            {
+                var worksheet = excelfile.Workbook.Worksheets[0];
+                totalRows = worksheet.Dimension.End.Row;
+                totalCols = worksheet.Dimension.End.Column;
+                for (int rowIndex = 1; rowIndex <= totalRows; rowIndex++)
+                {
+                    IEnumerable<string?> row = worksheet.Cells[rowIndex, 1, rowIndex, totalCols]
+                        .Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    List<string> tempRow = row!.ToList<string>();
+                    ExcelTable.Add(tempRow.ToArray());
+                }
+                return ExcelTable;
+            }
         }
     }
 }
--- console/Program.cs
+++ console/Program.cs
@@ -5,12 +5,22 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        List<string[]> a = ClassLibrary1.PreProc.ReadCsvFile("D:\\proc-c#\\console\\1.csv");
-        foreach(string[] c in a)
+        List<string[]> a = ClassLibrary1.PreProc.ReadCsvFile("C:\\Users\\pompa\\Source\\Repos\\PreProc\\console\\1.csv");
+        foreach (string[] c in a)
         {
-            foreach(string s in c)
+            foreach (string s in c)
             {
                 Console.Write(s + "     ");
+            }
+            Console.WriteLine();
+        }
+        List<string[]> b = ClassLibrary1.PreProc.ReadExcelFile("C:\\Users\\pompa\\Source\\Repos\\PreProc\\console\\book1.xlsx");
+        Console.WriteLine();
+        foreach (string[] v in b)
+        {
+            foreach (string n in v)
+            {
+                Console.Write(n + "     ");
             }
             Console.WriteLine();
         }
