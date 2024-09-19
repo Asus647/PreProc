18a1905931ebc1a6463066751946d17fb69700f1
Semyon Snastin <blessedornot@mail.ru>
2024-09-19 21:43:17 +0300

    hubububa

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,6 +2,23 @@
 {
     public static class PreProc
     {
-
+        /// <summary>
+        /// Читает и сохраняет данные из файла с расширением .csv
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns></returns>
+        public static List<string[]> ReadCsvFile(string path)
+        {
+            List<string[]> output = new List<string[]>();
+            StreamReader reader = new StreamReader(path);
+            string? line = reader.ReadLine();
+            while (line != null)
+            {
+                string[] values = line.Split(',');
+                output.Add(values);
+                line = reader.ReadLine();
+            }
+            return output;
+        }
     }
 }
---
+++ console/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        List<string[]> a = ClassLibrary1.PreProc.ReadCsvFile("D:\\proc-c#\\console\\1.csv");
+        foreach(string[] c in a)
+        {
+            foreach(string s in c)
+            {
+                Console.Write(s + "     ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
